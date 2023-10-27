aac2ccf7df94d2110ab22b1daa5f89d4191f23a2
tom-murray98 <118746667+tom-murray98@users.noreply.github.com>
2023-10-27 14:57:49 +0100

    Update minFQ_bam_handler.py

--- minFQ_bam_handler.py
+++ minFQ_bam_handler.py
@@ -2,18 +2,19 @@
 Very basic reader for BAM files and extracting fastq parameters
 import pysam
 """
+import pysam
+
 
 bam_file = "input.bam"  # place holder
-
 
 def read_bam():
     samfile1 = pysam.AlignmentFile(bam_file, "rb")
     for read1 in samfile1.fetch():
         # Defines fastq data for each read
-        qry_name = read1.query_name
+        name = read1.query_name
         desc = "+"
-        seq = read1.query_alignment_sequence
-        qual = read1.query_alignment_qualities
+        seq = read1.seq
+        qual = read1.qual
 
         yield qry_name, desc, seq, qual
 
