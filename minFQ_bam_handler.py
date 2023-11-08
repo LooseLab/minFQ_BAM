9427ef6f58f54d890bea6d4c8a8202c7f02dd979
tom-murray98 <118746667+tom-murray98@users.noreply.github.com>
2023-11-08 12:26:54 +0000

    Update minFQ_bam_handler.py
    Resolved issue with error reading ONT unaligned bams. Added the yielding of all available relevant data from header and read tags.

--- minFQ_bam_handler.py
+++ minFQ_bam_handler.py
@@ -4,39 +4,50 @@ import pysam
 def read_bam(bam_file=None):
     
     """
-    :param bam_file: A bam format alignment file.
-    :return:
-        name: sequence name
-        seq: sequence
-        qual: quality of reads
-        start_time_pr: start time of run (per read)
-        read_basecall_id: Read ID and basecall ID
-
-        run_id: id for run
-        flow_cell_id: id of flow cell
-        start_time: start time of run
-        basecall_method_run_id: Basecall method and run ID combined
+        :param bam_file: A bam format alignment file.
+        :return:
+            name: sequence name
+            seq: sequence
+            qual: quality of reads
+            start_time_pr: start time of run (per read)
+            read_basecall_id: Read ID and basecall ID
+    `       channel: channel ID
+            seq_to_signal :sequence to signal move table
+            dt: time of run
+            ds: run ID in basecall mode
+            lb: sample ID
+            pl: outputs 'ONT', device organisation?
+            pm: device position
+            pu: flow cell ID
+            al: unknown (outputs unclassified)
     """
 
     if bam_file:
         sam_file = pysam.AlignmentFile(bam_file, "rb", check_sq=False)
-        sequence_dict = sam_file.header.to_dict()['RG']
-        # Information from BAM header
-        for sequence_info in sequence_dict:
-            run_id = sequence_info['ID']
-            flow_cell_id = sequence_info['PU']
-            start_time = sequence_info['DT']
-            basecall_method_run_id = sequence_info['DS']
-        for bam_read in sam_file.fetch():
+        # Access the RG section of the header
+        rg_tags = sam_file.header.get("RG", [])
+        # Iterate through groups and extract tags
+        for rg_tag in rg_tags:
+            ident = rg_tag.get("ID")
+            dt = rg_tag.get("DT")  # Time of run
+            ds = rg_tag.get("DS")  # Run ID and basecall mode
+            lb = rg_tag.get("LB")  # Sample ID
+            pl = rg_tag.get("PL")  # outputs 'ONT', device organisation?
+            pm = rg_tag.get("PM")  # Device position
+            pu = rg_tag.get("PU")  # Flow cell ID
+            al = rg_tag.get("al")  # outputs 'unclassified'
+
+        for read in sam_file.fetch(until_eof=True):
             # From each read
-            name = bam_read.query_name
-            seq = bam_read.seq
-            qual = bam_read.qual
-            # Information from BAM tags in each read
-            start_time_pr = bam_read.get_tag('st')
-            channel = bam_read.get_tag('ch')
-            read_basecall_id = _bam_read.get_tag('RG')
-            # seq_to_signal = bam_read.get_tag('mv:B:c')
+            name = read.query_name
+            seq = read.seq
+            qual = read.qual
+            # From BAM tags in each read
+            start_time_pr = read.get_tag("st")
+            channel = read.get_tag("ch")
+            read_basecall_id = read.get_tag("RG")
+            seq_to_signal = read.get_tag("mv:B:c")
+
             yield (
                 name,
                 seq,
@@ -44,11 +55,17 @@ def read_bam(bam_file=None):
                 start_time_pr,
                 channel,
                 read_basecall_id,
-                run_id,
-                flow_cell_id,
-                start_time,
-                basecall_method_run_id,
+                seq_to_signal,
+                ident,
+                dt,
+                ds,
+                lb,
+                pl,
+                pu,
+                pm,
+                al,
             )
+
 
 for (
     name,
@@ -57,50 +74,33 @@ for (
     start_time_pr,
     channel,
     read_basecall_id,
-    run_id,
-    flow_cell_id,
-    start_time,
-    basecall_method_run_id,
-) in read_bam(bam_file="sort_ds1263_NUH7_M1.sup.meth.hg38.bam"):
-
-"""
-
-Parameters 
-----------
-name : str 
-    BAM read ID
-seq : str
-    Sequence string for BAM read
-qual : str
-    Quality string for BAM read
-start_time_per_read : str
-    Start time for each read
-channel : int
-    Channel for each read
-read_basecall_id : str
-    Basecall ID for each read
-run_id : str
-    ID for the run
-flow_cell_id : str
-    ID for the flow cell used
-start_time : str
-    Start time found in @RG (start of whole run?
-basecall_method_run_id: str
-    The basecall method used and run ID
-
-Returns
----------
-"""
-
-    bam_read = {"read_id": name,
-                "sequence": seq,
-                "sequence_length": len(str(seq)),
-                "quality": qual,
-                "start_time_per_read": start_time_pr,
-                "channel": channel,
-                "read_basecall_id": read_basecall_id,
-                "run_id": run_id,
-                "flow_cell_id": flow_cell_id,
-                "start_time": start_time,
-                "basecall_method_run_id": basecall_method_run_id
-               }
+    seq_to_signal,
+    ident,
+    dt,
+    ds,
+    lb,
+    pl,
+    pu,
+    pm,
+    al,
+) in read_bam(bam_file="230498_pass_68a2633f_385676fd_0.bam"):
+    bam_read = {
+        "id": ident,
+        "dt": dt,
+        "ds": ds,
+        "lb": lb,
+        "pl": pl,
+        "pu": pu,
+        "pm": pm,
+        "al": al,
+        "read_id": name,
+        "sequence": seq,
+        "sequence_length": len(str(seq)),
+        "quality": qual,
+        "start_time_per_run": start_time_pr,
+        "channel": channel,
+        "read_basecall_id": read_basecall_id,
+        "seq_to_signal": seq_to_signal,
+    }
+    print(bam_read)
+    break
